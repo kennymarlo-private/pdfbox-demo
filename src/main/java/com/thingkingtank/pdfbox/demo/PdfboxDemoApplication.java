14a5217546fae813f9a08bda612c7d72dece7e06
Kenny Marlo Cid <kennymarlo.private@gmail.com>
2019-12-01 20:43:06 +0800

    Initial Import

---
+++ src/main/java/com/thingkingtank/pdfbox/demo/PdfboxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.thingkingtank.pdfbox.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfboxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfboxDemoApplication.class, args);
+	}
+
+}
